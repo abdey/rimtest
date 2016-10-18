fb278ad8ae363b5f87c705ab4c8de51be1bcbc68
Abdérahman JAMAL @ AxaLifeInvest <abderahman.jamal@axa-lifeinvest.fr>
2016-10-19 00:14:03 +0200

    Add React Stack

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,10 +2,52 @@
 
 var browserSync = require('browser-sync'),
     gulp = require('gulp'),
-    proxyMiddleware = require('http-proxy-middleware');
+    gutil = require('gulp-util'),
+    proxyMiddleware = require('http-proxy-middleware'),
+    source  = require('vinyl-source-stream'),
+    babelify = require('babelify'),
+    watchify = require('watchify'),
+    exorcist = require('exorcist'),
+    browserify = require('browserify');
 
+// Watchify args contains necessary cache options to achieve fast incremental bundles.
+// See watchify readme for details. Adding debug true for source-map generation.
+watchify.args.debug = true;
+// Input file.
+var bundler = watchify(browserify('./src/webapp/app.jsx', watchify.args));
 
-gulp.task('serve', function () {
+// Babel transform
+bundler.transform(babelify.configure({
+    sourceMapRelative: 'src/webapp'
+}));
+
+// On updates recompile
+bundler.on('update', bundle);
+
+function bundle() {
+
+    gutil.log('Compiling JS...');
+
+    return bundler.bundle()
+        .on('error', function (err) {
+            gutil.log(err.message);
+            browserSync.notify("Browserify Error!");
+            this.emit("end");
+        })
+        .pipe(exorcist('src/webapp/dist/bundle.js.map'))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./src/webapp/dist'))
+        .pipe(browserSync.stream({once: true}));
+}
+
+/**
+ * Gulp task alias
+ */
+gulp.task('bundle', function () {
+    return bundle();
+});
+
+gulp.task('serve', ['bundle'], function () {
     browserSync.instance = browserSync.init({
         startPath: '/',
         server: {
