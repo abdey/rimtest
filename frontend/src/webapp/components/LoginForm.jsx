732b972e6b96d9fb82d338c330c20755efd36cf1
Abdérahman JAMAL <abdey@jamal.co>
2016-10-19 07:43:18 -0400

    Add Login Component Style

--- frontend/src/webapp/components/LoginForm.jsx
+++ frontend/src/webapp/components/LoginForm.jsx
@@ -4,7 +4,24 @@ import ReactDOM from 'react-dom';
 export default class LoginForm extends React.Component {
   render() {
     return (
-      <h1>Login Page</h1>
+      <div className="container">
+          <div className="row">
+              <div className="col-xs-12 col-md-6 col-md-offset-3">
+                  <div className="form-container">
+                      <form className="form-signin">
+                        <label> Login </label>
+                        <input type="text" className="form-control" placeholder="Email" />
+                        <input type="password" className="form-control" placeholder="Password" />
+                        <button className="btn btn-lg btn-primary btn-block" type="submit">
+                            Login
+                        </button>
+                        <span className="clearfix"></span>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+
     )
   }
 }
